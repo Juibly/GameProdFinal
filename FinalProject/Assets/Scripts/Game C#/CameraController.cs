47fa77ad499c02722fad3331c3e4c17affb6b2e8
MasonKuhn <masonkuhn.1804@gmail.com>
2025-03-23 17:03:14 -0400

    Update CameraController.cs

--- FinalProject/Assets/Scripts/Game C#/CameraController.cs
+++ FinalProject/Assets/Scripts/Game C#/CameraController.cs
@@ -22,9 +22,9 @@ public class CameraController : MonoBehaviour
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        float horizationInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizationInput;
+        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         if (inputDir != Vector3.zero)
         {
