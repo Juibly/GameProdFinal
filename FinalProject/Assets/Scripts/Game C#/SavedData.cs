026342e500fe2cfa8196ccb185958e0f01294885
InfiniumKnight <nasaman1954@gmail.com>
2025-04-12 12:39:45 -0400

    data saving added and functionality confirmed

---
+++ FinalProject/Assets/Scripts/Game C#/SavedData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavedData : MonoBehaviour
+{
+    //best times for each level, game opens with them all being set to 60. perhaps in the best time display in the hub,
+    //we can have it check for if the best time is 60 or above and not show it. or only show best time after the levels have been beaten.
+    public static float rooftopBestTime = 60;
+    public static float sewerBestTime = 60;
+    public static float apartmentBestTime = 60;
+
+    //bools for level completion to be used as needed.
+    public static bool rooftopCompletion = false;
+    public static bool sewerCompletion = false;
+    public static bool apartmentCompletion = false;
+}
