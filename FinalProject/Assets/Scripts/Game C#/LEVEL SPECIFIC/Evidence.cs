e0eca78bed8ee63755b8bf702f9caebc51d494e5
Juibly <jakergreig@gmail.com>
2025-04-21 14:29:49 -0400

    collision fixes and fuseboxes
    complete description in discord

--- FinalProject/Assets/Scripts/Game C#/LEVEL SPECIFIC/Evidence.cs
+++ FinalProject/Assets/Scripts/Game C#/LEVEL SPECIFIC/Evidence.cs
@@ -16,7 +16,7 @@ public class Evidence : MonoBehaviour
     void Start()
     {
         evidenceCount = 0; //initialize evidence collected
-        evidenceTotal = 4;
+        evidenceTotal = 3;
         evidenceFinished = false; //initialize win condition for level
     }
 
