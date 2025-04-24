1568632162fc3fe8b074b6b82dc5a2c2c6a95146
Cecil <cecil.creates@gmail.com>
2025-04-24 19:07:49 -0400

    beta 2
    APARTMENT COMPLETE
    added pop up menu to options selection on main menu, made new main menu with new kevin art, fixed sizing in main menu and how to play, how to play has working toggle showing keyboard and controller keys with an on off switch, further organization ect

--- FinalProject/Assets/Scripts/Menu C#/mainMenu.cs
+++ FinalProject/Assets/Scripts/Menu C#/mainMenu.cs
@@ -5,30 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
+    //creates don't destroy on load
     [SerializeField] private GameObject gameManagerPrefab;
     private GameManager gameManager;
+
+
+    public GameObject menuScreen;
+    public GameObject optionScreen;
     void Start()
     {
-        
+        optionScreen.SetActive(false);
+        menuScreen.SetActive(true);
     }
 
 
+//OPTION SELECTION
+public void back(){ //if say no to freeplay options screen goes away, and menu is back to normal
+        optionScreen.SetActive(false);
+        menuScreen.SetActive(true);
+
+}
+
 //MENU SELECTIONS
     public void loadMainMenu(){
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     //OPENS HUB
     public void playGame(){
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
 
     public void howToPlay(){
         SceneManager.LoadScene(2);
     }
 
-    public void settings(){
-        SceneManager.LoadScene(3);
+    public void settings(){ //turns on options pop up selection and turns off main menu screen
+        optionScreen.SetActive(true);
+        menuScreen.SetActive(false);
     }
     public void quit(){
         Application.Quit();
--- FinalProject/Assets/Scripts/Menu C#/otherMenuSelection.cs
+++ FinalProject/Assets/Scripts/Menu C#/otherMenuSelection.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class otherMenuSelection : MonoBehaviour
 {
+
+//objects for turning on and off showing keyboard/controller controls
+    public Toggle checkController; //checklist-truefalse
+    public GameObject controlParent; // what shows controller controls
+    public GameObject keyboardParent; // what shows keyboard controls
+
     // Start is called before the first frame update
     void Start()
     {
+        checkController.isOn = false; //toggle is off from start
         Cursor.lockState = CursorLockMode.None;
     }
 
@@ -16,6 +23,20 @@ public class otherMenuSelection : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.None;
     }
+
+//on howToPlay Scene
+public void showController(){
+    
+    if(checkController.isOn == false){
+        keyboardParent.SetActive(true);//on start is false, keyboard true
+        controlParent.SetActive(false);
+    } 
+    else{
+        keyboardParent.SetActive(false);//when click toggle keyboard false
+        controlParent.SetActive(true);
+    }
+
+}
 
     //MENU SELECTIONS
     public void loadMainMenu(){
