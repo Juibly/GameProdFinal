8df3d5a4c6de9de8ebecea939d86411c66899e4a
Juibly <jakergreig@gmail.com>
2025-04-25 01:07:04 -0400

    menu stuff

---
+++ FinalProject/Assets/Scripts/OptionClick.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionClick : MonoBehaviour
+{
+    public GameObject OptionClick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
