51c88c52b77b8a93cc54391131afad81869bc627
Vee Czyscon <ma191433@ucf.edu>
2025-04-25 19:55:48 -0400

    Interact Work on Controller
    Update code to use input manager over keycodes

--- FinalProject/Assets/Scripts/Game C#/interactScript.cs
+++ FinalProject/Assets/Scripts/Game C#/interactScript.cs
@@ -9,8 +9,9 @@ public class interactScript : MonoBehaviour
     [SerializeField] GameObject PickupCollider;
 
 
-    [Header("Keybinds")] //Variables for Keybinds
-    public KeyCode pickupKey = KeyCode.E; // E to interact
+    //[Header("Keybinds")] //Variables for Keybinds
+    //public KeyCode pickupKey = KeyCode.E; // E to interact
+    //this was changed to instead use the input manager with the button "PickUp"
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +24,10 @@ public class interactScript : MonoBehaviour
     {
         PickupCollider.SetActive(pickUp); // this changes if the collision for pickup is active or not based on bool
 
-        if (Input.GetKeyDown(pickupKey))
+        if (Input.GetButton("PickUp"))
         {
             pickUp = true; //activates collision for picking stuff up
         }
-
-        if (Input.GetKeyUp(pickupKey)) { pickUp = false; } // if not picking something up, remove the collision for that
+        else { pickUp = false; } // if not picking something up, remove the collision for that
     }
 }
